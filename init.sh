6b360104ff5b494e243d6d2b6855cb483008f73a
Shahar Levin <Shahar@ILT.co.il>
2019-07-29 23:50:25 +0300

    Basic

---
+++ init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+RUN ln -sf /dev/stdout /var/ossec/logs/ossec.log
+/var/ossec/bin/agent-auth -m ad209f5bda7a011e98e650269ba31080-1608765567.eu-west-1.elb.amazonaws.com
+/etc/init.d/wazuh-agent start
+STATUS_CMD="/etc/init.d/wazuh-agent status"
+
+while true
+do
+  eval $STATUS_CMD > /dev/null
+  if (( $? != 0 ))
+  then
+    CUR_TIME=`date +%s`
+    # Allow ossec to not run return an ok status for up to 15 seconds
+    # before worring.
+    if (( (CUR_TIME - LAST_OK_DATE) > 15 ))
+    then
+      echo "ossec not properly running! exiting..."
+      ossec_shutdown
+      exit 1
+    fi
+  else
+    LAST_OK_DATE=`date +%s`
+  fi
+  sleep 1
+done
